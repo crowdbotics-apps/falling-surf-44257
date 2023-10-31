048f2077675f4502e933b55d31d626ca6e7483a9
Crowdbotics <team@crowdbotics.com>
2023-10-31 19:58:42 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "falling-surf-44257",
+  url: "https://falling-surf-44257.botics.co",
+  api: "https://falling-surf-44257.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
